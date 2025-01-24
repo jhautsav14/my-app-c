6f93fc26c9d756dd95fcffd7074d76cfdcf62dcc
Utsav Kumar <95420144+jhautsav14@users.noreply.github.com>
2025-01-25 03:27:49 +0530

    solved2

--- app/appointments/book/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BookAppointmentPage() {
-    return (
-        <div>
-            <h1>Book an Appointment</h1>
-            {/* Add your form or content here */}
-        </div>
-    );
-}
